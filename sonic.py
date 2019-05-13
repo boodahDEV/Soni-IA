eca656ea2db40ccdb215420d24c68df0914cc740
boodah <boodah21@protonmail.com>
2019-05-13 01:53:54 -0500

    Trabajando sobre controladores. Mapeo de datos.

--- sonic.py
+++ sonic.py
@@ -3,7 +3,7 @@ import os
 #from Controler_actions import action_control
 env = retro.make('SonicTheHedgehog-Genesis', 'GreenHillZone.Act1')
 
-f = open ('data.txt','w')
+f = open ('data.csv','w')
 
 env.reset()
 done = False
@@ -12,8 +12,12 @@ while not done:
     action = env.action_space.sample()
     action = [0,0,0,0,0,0,0,1,0,0,0,0]
     ob, rew, done, info = env.step(action)
-    f.write("\n"+str(action))
-    #print("Action: ",info, "\tDone: ", done, "\tOb: ", ob) 
+    f.write("\n"+str(info))
+    #ob es la imagen de la pantalla en el momento de la accion
+    #done es para saber suantas vidas tiene o que, osea si muere done == true se cierra el bucle!
+    #info es un diccionario de todos los valores establecidos en los datos correctos.
+    print("Image: ",ob.shape,"\tReward: ",rew, "\tDone: ", done, "\t actions: ", action) 
+    
 
 f.close()
 #print("que haces:", info)
