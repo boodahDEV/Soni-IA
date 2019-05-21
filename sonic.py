849be56f70bb90cb607c1aa5601ec5a88614a550
boodah <boodah21@protonmail.com>
2019-05-21 16:34:18 -0500

    Se finaliza el controlador para el mundo de Sonic.

--- sonic.py
+++ sonic.py
@@ -2,19 +2,20 @@ import retro
 import pygame
 import os
 from pygame.locals import *
-
-
-video_size = 1, 1
+#video_size = 1, 1
 env = retro.make('SonicTheHedgehog-Genesis', 'GreenHillZone.Act1')
-screen = pygame.display.set_mode(video_size)
+#screen = pygame.display.set_mode(video_size)
 
-env.reset()
+clock = pygame.time.Clock() #controla los fps del juego
+obs = env.reset()
+pygame.init()
+win = pygame.display.set_mode((800,600))
+
 
 done = False
 def key_action():
     keys=pygame.key.get_pressed()
     key = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    
     if keys[K_LEFT]:
         key[6] = 1
     if keys[K_UP]:
@@ -28,12 +29,24 @@ def key_action():
 
 archivo = open("data_game.csv", "a")
 while not done:
-    img = env.render()
-    screen = pygame.display.set_mode(video_size)
-    pygame.display.set_caption(str(img))
-    #pygame.display.update()
+    #env.render()
+    
+    #screen = pygame.display.set_mode(video_size)
+    #pygame.display.set_caption(str(img))
+    win = pygame.display.set_mode((800,600))
+
+
+    # Display
+    img = pygame.image.frombuffer(obs.tostring(), obs.shape[1::-1], 'RGB')
+    img = pygame.transform.scale(img, (800,600))
+    win.blit(img,(0,0))
+    pygame.display.flip()
+
+
+    #Este permite la ejecucion y control por teclado
     action = key_action()
-    ob, rew, done, info = env.step(action)
+    obs, rew, done, info = env.step(action)
+
     archivo.write(str(info))
     archivo.write(";")
     archivo.write(str(action))
@@ -41,4 +54,6 @@ while not done:
     archivo.write(str(rew))
     archivo.write(";") #columnas
     archivo.write("\n")
-    #print("Action ", action, "Reward ", rew)
+
+   # print("Action ", action, "Reward ", rew)
+clock.tick(500)
